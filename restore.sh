9a43ca42c093910de821b1b2f08d3b4a5bec87a2
kushlychok <kushlychok@gmail.com>
2019-08-06 20:18:34 +0300

    Create restore.sh

---
+++ restore.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#to run this script you need to know the date for example 05_08_2019 to run it: ./restore.sh 05_08_2019
+export PGPASSWORD=`aws ssm get-parameters --name DBpassPG --region us-west-2 --with-decryption --output text --query Parameters[].Value`
+file=$1
+echo $1
+cd /root/backup
+aws s3 cp s3://backupgitlab-opsworks/$file.tar.gz /root/backup
+tar xvzf $file.tar.gz
+
+cd /root/backup/$file
+
+cp -r gitlab /data
+
+psql -U adminpg -h 127.0.0.1 -p 5432 -d gitlab < gitlab.dump
